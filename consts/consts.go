bac5733979fb10f664295c22bed1b1044b821ffe
Arya390akku <aryaofficial390@gmail.com>
2024-09-11 04:18:19 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "ARYA"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "AARYHAA"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ARY3"
 )
 
 var ID ids.ID
